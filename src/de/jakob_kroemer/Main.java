cda1dd87e79adb8c3a6c9ef2ee76dbdbc19cdbc5
feris <jakob.kroemer@gmx.de>
2023-01-17 16:08:20 -0500

    Update Q6

--- src/de/jakob_kroemer/Main.java
+++ src/de/jakob_kroemer/Main.java
@@ -1,57 +1,18 @@
 package de.jakob_kroemer;
 import org.apache.spark.api.java.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.function.DoubleFunction;
 import org.apache.spark.api.java.function.Function;
 
 public class Main {
-  public static void main(String[] args) {
+	public static void main(String[] args) {
 	
-	String logFile 				= "./NY-02_short.csv"; // Should be some file on your system
-    SparkConf conf 				= new SparkConf().setMaster("local").setAppName("Simple Application");
-    JavaSparkContext sc 		= new JavaSparkContext(conf);
-    JavaRDD<String> logData 	= sc.textFile(logFile).cache();
-    
-    /**
-    0 	= medallion
-    1 	= hack_license
-    2 	= vendor_id
-    3 	= rate_code
-    4 	= store_and_fwd_flag
-    5 	= pickup_datetime
-    6 	= dropoff_datetime
-    7 	= passenger_count
-    8 	= trip_time_in_secs
-    9 	= trip_distance
-    10 	= pickup_longitude
-   	11 	= pickup_latitude
-    12 	= dropoff_longitude
-   	13 	= dropoff_latitude
-   	14 	= medallion
-    15 	= hack_license
-    16 	= vendor_id
-    17 	= pickup_datetime
-    18 	= payment_type
-    19 	= fare_amount
-    20 	= surcharge
-    21 	= mta_tax
-    22	= tip_amount
-    23 	= tolls_amount
-    24 	= total_amount 
-    */
-    JavaRDD<String[]> splitData = logData.map(new Function<String, String[]>() {
-        public String[] call(String s) { 
-        	String[] array = s.split(","); 
-        	return array;
-        }
-    });
-    
-    //Q3 Q3 = new Q3(logData);
-
-    System.out.println("Test" + splitData.toString());
-  }
+		Q6 Q6 = new Q6();
+	}
 }
--- src/de/jakob_kroemer/Q6.java
+++ src/de/jakob_kroemer/Q6.java
@@ -3,17 +3,69 @@
  */
 package de.jakob_kroemer;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaDoubleRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.DoubleFunction;
+
 /**
  * @author osboxes
  *
  */
-public class Q6 {
-
+public class Q6 implements Serializable{
+	
 	/**
 	Ermittle ob und wie das Trinkgeld von der Uhrzeit abhängt.
 	*/
 	public Q6() {
-		// TODO Auto-generated constructor stub
-	}
+		String logFile 				= "/home/osboxes/eclipse-workspace/Business-Intelligence2/src/NY-02_short.csv"; // Should be some file on your system
+		SparkConf conf 				= new SparkConf().setMaster("local").setAppName("Simple Application");
+		JavaSparkContext sc 		= new JavaSparkContext(conf);
+		JavaRDD<String> logData 	= sc.textFile(logFile).cache();
+      
+		JavaRDD<String> tipAmount = logData.map(new Function<String, String>() {
+			public String call(String s) { 
+				String[] attributes = s.split(",");
+				String total = "\n"+"Zeit:" + attributes[5].substring(11) + "," +  attributes[22];
+				return total;
+			}
+		});
 
+		ArrayList<JavaDoubleRDD> results = new ArrayList<>();
+		for (int i = 0; i < 24; i++) {
+			String number = i < 10 ? "0" + i : i + "";
+			
+			JavaRDD<String> timeFilter = tipAmount.filter(new Function<String, Boolean>() {
+				public Boolean call(String s) { 
+					return s.contains("Zeit:" + number + ":") ; 
+				}
+			});
+		 
+			JavaRDD<Double> avgTip = timeFilter.map(new Function<String, Double>() {
+				public Double call(String s) { 
+					String[] attributes = s.split(",");
+					String total = attributes[1];
+					return Double.parseDouble(total);
+				}
+			});
+		 
+			JavaDoubleRDD result = avgTip.mapToDouble(new DoubleFunction<Double>() {
+				public double call(Double x) {
+					return (double) x;
+				}
+			});
+			results.add(result);
+		}
+		
+		for (int i = 0; i < 24; i++) {
+			System.out.println("Trinkgeld zwischen " + i + "-" + (i+1) + " Uhr: " + results.get(i).mean());
+		}
+	};
+	
 }
+
