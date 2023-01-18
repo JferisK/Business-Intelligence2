3d3457b80f95a6146502738b06aa446527d0329a
feris <52134775+JferisK@users.noreply.github.com>
2023-01-18 10:06:05 +0100

    Remove reading data

--- src/de/jakob_kroemer/Q2.java
+++ src/de/jakob_kroemer/Q2.java
@@ -23,21 +23,15 @@ import org.apache.spark.api.java.function.FlatMapFunction;
 
 public class Q2 implements Serializable{ 
 	
-	public Q2() {
+	JavaRDD<String> logData;
+	
+	public Q2(JavaRDD<String> logData) {
+		this.logData = logData;
 		System.out.println("Brechne Q2");
 	};
 	
 	
 	public void calcResult() {
-		
-		
-		String logFile = "/home/osboxes/data/NY_medium2_UTF8.csv"; 
-		SparkConf conf = new SparkConf().setMaster("local").setAppName("Simple Application");
-		JavaSparkContext sc = new JavaSparkContext(conf);
-		
-		//JavaRDD<String> logData = sc.textFile(logFile).cache();
-		JavaRDD<String> logData = sc.textFile(logFile);
-		
 		JavaRDD<Double> pickup_lat = logData.map(new Function<String, Double>() {
 			
 			public Double call(String s) {
