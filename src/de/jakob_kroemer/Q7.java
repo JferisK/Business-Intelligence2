2489abe859710659089e193a84cf10d194c2b5c4
NoFr02 <93268500+NoFr02@users.noreply.github.com>
2023-01-19 13:55:12 -0500

    Change Q7 and Q8

--- src/de/jakob_kroemer/Q7.java
+++ src/de/jakob_kroemer/Q7.java
@@ -31,42 +31,42 @@ public class Q7 implements Serializable{
 	*/
 	public Q7(JavaRDD<String> logData) {
 		this.logData = logData;
-		System.out.println("Berechne Q7");
+		System.out.println("Berechne Q8");
 	};
 	
 	public void calcResult() {	
 		
-	PairFunction<String, Integer, Double> pair = new PairFunction<String, Integer, Double>() {
+	PairFunction<String, Long, Double> pair = new PairFunction<String, Long, Double>() {
 
-        public Tuple2<Integer, Double> call(String s) {
+        public Tuple2<Long, Double> call(String s) {
 	        String[] attributes = s.split(",");
-	        int minutes = Math.round(Integer.parseInt(attributes[7])/60);
-	       	return new Tuple2(minutes, Double.parseDouble(attributes[attributes.length-3]));               
+	        long distance =  Math.round(Integer.parseInt(attributes[8])/10.0) * 10;
+	       	return new Tuple2(distance, Double.parseDouble(attributes[attributes.length-3]));               
         }
 	};
 	    	
-    JavaPairRDD<Integer, Double> pairs = logData.mapToPair(pair);
+    JavaPairRDD<Long, Double> pairs = logData.mapToPair(pair);
     //count each values per key
-    JavaPairRDD<Integer, Tuple2<Double, Integer>> valueCount = pairs.mapValues(value -> new Tuple2<Double,Integer>(value,1));
+    JavaPairRDD<Long, Tuple2<Double, Integer>> valueCount = pairs.mapValues(value -> new Tuple2<Double,Integer>(value,1));
 
     //add values by reduceByKey
-    JavaPairRDD<Integer, Tuple2<Double, Integer>> reducedCount = valueCount.reduceByKey((tuple1,tuple2) ->  new Tuple2<Double, Integer>(tuple1._1 + tuple2._1, tuple1._2 + tuple2._2));
+    JavaPairRDD<Long, Tuple2<Double, Integer>> reducedCount = valueCount.reduceByKey((tuple1,tuple2) ->  new Tuple2<Double, Integer>(tuple1._1 + tuple2._1, tuple1._2 + tuple2._2));
     for(int i =0; i < reducedCount.collect().size(); ++i) {
 	System.out.println("Anzahl Daten:"+reducedCount.collect().get(i)._1 + " :: " +reducedCount.collect().get(i)._2._2); 
 	}
     //calculate average
-    PairFunction<Tuple2<Integer, Tuple2<Double, Integer>>,Integer,Double> getAverageByKey = (tuple) -> {
+    PairFunction<Tuple2<Long, Tuple2<Double, Integer>>,Long,Double> getAverageByKey = (tuple) -> {
     	Tuple2<Double, Integer> val = tuple._2;
     	double total = val._1;
     	double count = val._2;
-    	Tuple2<Integer, Double> avgTip = new Tuple2<Integer, Double>(tuple._1, total / count);
+    	Tuple2<Long, Double> avgTip = new Tuple2<Long, Double>(tuple._1, total / count);
     	return avgTip;
     };
-    JavaPairRDD<Integer, Double> avgTip = reducedCount.mapToPair(getAverageByKey);
+    JavaPairRDD<Long, Double> avgTip = reducedCount.mapToPair(getAverageByKey);
     for(int i =0; i < avgTip.collect().size(); ++i) {
     	System.out.println("Durchschnitt Trinkgeld: "+avgTip.collect().get(i)._1 + " :: " +avgTip.collect().get(i)._2); 
     	}
-		System.out.print("Q7 Done!");
+		System.out.print("Q8 Done!");
 	}
 	
 }
