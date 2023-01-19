ff119694d4eb4479a130d7369a660e9a4460c6d4
NoFr02 <93268500+NoFr02@users.noreply.github.com>
2023-01-19 03:53:42 -0500

    Update Merge.java

--- src/de/jakob_kroemer/Merge.java
+++ src/de/jakob_kroemer/Merge.java
@@ -19,14 +19,18 @@ public class Merge {
 		
 	    SparkConf conf = new SparkConf().setMaster("local").setAppName("Simple Application");
 	    JavaSparkContext sc = new JavaSparkContext(conf);
+	    long data1count = sc.textFile(file1).count();
+	    System.out.println(data1count);
+	    long data2count = sc.textFile(file2).count();
+	    System.out.println(data2count);
 	    JavaRDD<String> data1 = sc.textFile(file1).mapPartitionsWithIndex((index, iterator) -> {
-            if (index == 0) {
+            if (index == 0 || index == data1count-1) {
                 iterator.next();
             }
             return iterator;
         }, true).cache();
 	    JavaRDD<String> data2 = sc.textFile(file2).mapPartitionsWithIndex((index, iterator) -> {
-            if (index == 0) {
+            if (index == 0 || index == data2count-1 ) {
                 iterator.next();
             }
             return iterator;
@@ -36,38 +40,40 @@ public class Merge {
 	    
 	    //System.out.println("hey:"+data2.collect().get(1));
 	    //create pairs with key: passengerNumber values: tipAmount
-	    PairFunction<String, String, String> keyData = 
-	    		new PairFunction<String, String, String>() {
-	    			public Tuple2<String, String> call(String s) {
-	    				String[] attributes = s.split(",");
-	    								 //    key			 , 				value
-	    				return new Tuple2(attributes[0]+":"+attributes[1], attributes[2]+","+attributes[3]+","+attributes[4]+","+attributes[5]+","
-	    								 +attributes[6]+","+attributes[7]+","+attributes[8]+","+attributes[9]+","+attributes[10]+","+attributes[11]
-	    										 );//","+attributes[12]+","+attributes[13]
-	    			}
-	    		}; 
-	    PairFunction<String, String, String> keyFare = 
-	    		new PairFunction<String, String, String>() {
-	    			public Tuple2<String, String> call(String s) {
-	    				String[] attributes = s.split(",");
-	    								 //    key			 , 				value
-	    				return new Tuple2(attributes[0]+":"+attributes[1], attributes[2]+","+attributes[3]+","+attributes[4]+","+attributes[5]+","
-								 +attributes[6]+","+attributes[7]+","+attributes[8]+","+attributes[9]+","+attributes[10]);
-	    			}
-	    		};
-	    
-	    		
-	    JavaPairRDD<String,String> pairs1 = data1.mapToPair(keyData);
-	    JavaPairRDD<String,String> pairs2 = data2.mapToPair(keyFare);
-	    JavaPairRDD<String,Tuple2<String, String>>  result = pairs1.join(pairs2);
-	    
-	    Function flatten = new Function<Tuple2<String,Tuple2<String,String>>,String>(){
-	    	public String call(Tuple2<String,Tuple2<String,String>> a) {
-	    		return (a._1 +","+a._2._1+","+a._2._2);}
+		
+	    PairFunction<String, String, String> keyData = new PairFunction<String,String, String>() { 
+	    	public Tuple2<String, String> call(String s) { 
+	    		String[]attributes = s.split(","); // key , value 
+	    		System.out.println(attributes.toString());
+	    		return new Tuple2(s.split(",")[0]+":"+s.split(",")[1]+":"+s.split(",")[5],s.split(",")[2]+","+s.split(",")[3]+","+s.split(",")[4]+","+s.split(",")[5]+","
+	    				+s.split(",")[6]+","+s.split(",")[7]+","+s.split(",")[8]+","+s.split(",")[9]+","+s.split(",")[10]+","
+	    				+s.split(",")[11]+","+s.split(",")[12]+","+s.split(",")[13]); 
+	    		} 
 	    	};
-	    	
+	   
+	    PairFunction<String, String, String> keyFare = new PairFunction<String,String, String>() { 
+	    	public Tuple2<String, String> call(String s) { 
+	    		//String[]attributes = s.split(","); // key , value 
+	    		return new Tuple2(s.split(",")[0]+":"+s.split(",")[1]+":"+s.split(",")[3],s.split(",")[2]+","+s.split(",")[3]+","+s.split(",")[4]+","+s.split(",")[5]+","
+	    		+s.split(",")[6]+","+s.split(",")[7]+","+s.split(",")[8]+","+s.split(",")[9]+","+
+	    		s.split(",")[10]);
+	    	}
+	    };
+		  
+		  
+		  JavaPairRDD<String,String> pairs1 = data1.mapToPair(keyData);
+		  JavaPairRDD<String,String> pairs2 = data2.mapToPair(keyFare);
+		  JavaPairRDD<String,Tuple2<String, String>> result = pairs1.join(pairs2);
+		  System.out.println(result.count()); 
+		  
+		  Function flatten = new Function<Tuple2<String,Tuple2<String,String>>,String>(){ 
+			  public String call(Tuple2<String,Tuple2<String,String>> a) { 
+				  return (a._1+","+a._2._1+","+a._2._2);} };
+		  
+		  
+		  JavaRDD<String> output = result.map(flatten);
+		 
 	    
-	    JavaRDD<String> output = result.map(flatten);
 	    return output;
 	  }
 }
